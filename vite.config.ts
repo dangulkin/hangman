97818a43d8266671b5b7456181de4e06647972c0
d.gulyaikin <d.gulyaikin@gmail.com>
2024-07-15 11:40:19 +0000

    vite config updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  root: "./",
-  build: {
-    outDir: "dist",
+  server: {
+    port: 3000,
   },
-  publicDir: "public",
-  base: './'
-});
+  plugins: [
+    vue()
+  ],
+  resolve: {
+    alias: [
+      { find: '/@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+    ],
+    extensions: ['.js','.ts','.json','.jsx','.mjs','.tsx','.vue']
+  },
+  base: '/'
+})
