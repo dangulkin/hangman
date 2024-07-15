5665e157fd33cbb9c768785fec1f6d3b121552d8
d.gulyaikin <d.gulyaikin@gmail.com>
2024-07-15 20:51:00 +0000

    Imports in App.vue updated

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,44 @@
 <script setup lang="ts">
-import GamePopup from "./components/GamePopup.vue";
-import { GameStatus } from "./types/GameStatus.ts";
-import { ref, watch } from "vue";
-import { useRandomWord } from "./composables/useRandomWord.ts";
-import { useLetters } from "./composables/useLetters.ts";
-import { useNotification } from "./composables/useNotification.ts";
+import GameHeader from './components/GameHeader.vue';
+import GameFigure from './components/GameFigure.vue';
+import GameWrongLetters from './components/GameWrongLetters.vue';
+import GameWord from './components/GameWord.vue';
+import GamePopup from './components/GamePopup.vue';
+import GameNotification from './components/GameNotification.vue';
 
-const {theName, getRandomWord} = useRandomWord();
-const {letters, correctLetters, wrongLetters, isWin, isLose, addLetter, resetLetters} = useLetters(theName);
+import { GameStatus } from './types/GameStatus.ts';
+import { ref, watch } from 'vue';
+import { useRandomWord } from './composables/useRandomWord.ts';
+import { useLetters } from './composables/useLetters.ts';
+import { useNotification } from './composables/useNotification.ts';
+
+const { theName, getRandomWord } = useRandomWord();
+const { letters, correctLetters, wrongLetters, isWin, isLose, addLetter, resetLetters } = useLetters(theName);
 const { notification, showNotification } = useNotification();
 const popup = ref<InstanceType<typeof GamePopup> | null>(null);
 
 const openPopup = (status: GameStatus) => {
   popup.value?.open(status);
-}
+};
 
 const restart = async () => {
   await getRandomWord();
   resetLetters();
   popup.value?.close();
   notification.value?.close();
-}
+};
 
 watch(wrongLetters, () => {
   if (isLose.value) {
     openPopup('lose');
   }
-})
+});
 
 watch(correctLetters, () => {
   if (isWin.value) {
     openPopup('win');
   }
-})
+});
 
 window.addEventListener('keydown', ({ key }) => {
   if (isWin.value || isLose.value) {
@@ -45,12 +51,13 @@ window.addEventListener('keydown', ({ key }) => {
   }
 
   addLetter(key);
-})
+});
 
 defineExpose({
   restart
 });
 </script>
+
 <template>
   <GameHeader />
   <div class="game-container">
