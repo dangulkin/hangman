92be843d3ea841acc7136eadd18c93c86de6d804
d.gulyaikin <d.gulyaikin@gmail.com>
2024-07-15 10:51:52 +0000

    vite config updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  root: './',
-    build: {
-        outDir: 'dist',
-    },
-    publicDir: 'public'
-})
-
+  root: "./",
+  build: {
+    outDir: "dist",
+  },
+  publicDir: "public",
+  base: './'
+});
