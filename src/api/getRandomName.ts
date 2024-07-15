e2096d217572fc34c2fa5d0e088d34a233088265
d.gulyaikin <d.gulyaikin@gmail.com>
2024-07-15 08:12:35 +0000

    Drop files from .gitignore

--- src/api/getRandomName.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const getRandomName = async () => {
-  const { data } = await axios.get(
-    "https://randomuser.me/api/?inc=name&nat=us"
-  );
-  return data.results[0].name.first;
-};
--- src/components/GameHeader.vue
+++
@@ -1,4 +0,0 @@
-<template>
-    <h1>Hangman</h1>
-    <p>Guess the name - enter a letter</p>
-</template>
